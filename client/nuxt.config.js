05ff3fbac54a87e630aea1f86c3ddcd09faa004e
baisheng <baisheng@gmail.com>
2018-12-18 18:59:51 +0800

    update

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -94,9 +94,9 @@ module.exports = {
     // },
     // analyze: true
     // 设置 CDN 地址，将 nuxt 内容传至 cdn 加速
-    // publicPath: 'http://spring.chinauff.com/spring/_nuxt/',
+    publicPath: 'http://spring.chinauff.com/spring/_nuxt/',
     // publicPath: apiConfig.cdnUrl + '/_nuxt/',
-    publicPath: '/spring/_nuxt/',
+    // publicPath: '/spring/_nuxt/',
     postcss: {
       // Add plugin names as key and arguments as value
       // Install them before as dependencies with npm or yarn
