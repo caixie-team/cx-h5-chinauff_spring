78dbec4d7dda42d23cada37d308abed061d02a09
baisheng <baisheng@gmail.com>
2018-12-18 18:34:50 +0800

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,14 @@
+/* eslint-disable no-undef */
+const fs = require('fs')
+let PORT = null
+const portFile = think.ROOT_PATH + '/PORT'
+if (think.isFile(portFile)) {
+  PORT = fs.readFileSync(portFile, 'utf8')
+}
 // default config
 module.exports = {
-  workers: 1
-};
+  // http_: 1, // 1:http,2:https
+  workers: 1,
+  host: '0.0.0.0',
+  port: PORT || 8360,
+}
