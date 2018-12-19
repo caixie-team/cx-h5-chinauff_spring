bc2490bf4812baa09ad43b574b17a1a69350acd1
baisheng <baisheng@gmail.com>
2018-12-19 11:16:16 +0800

    update

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -94,8 +94,8 @@ module.exports = {
     // },
     // analyze: true
     // 设置 CDN 地址，将 nuxt 内容传至 cdn 加速
-    // publicPath: 'https://weixin.chinauff.com/spring/_nuxt/',
-    publicPath: '/spring/_nuxt/',
+    publicPath: 'https://weixin.chinauff.com/spring/_nuxt/',
+    // publicPath: '/spring/_nuxt/',
     // publicPath: apiConfig.cdnUrl + '/_nuxt/',
     // publicPath: '/spring/_nuxt/',
     postcss: {
