541683ac4c13e67fefb339e092f55ff122577a42
baisheng <baisheng@gmail.com>
2018-12-28 00:32:05 +0800

    update api

--- client/api.config.js
+++ client/api.config.js
@@ -6,11 +6,13 @@
 const { isProdMode } = require('./environment')
 
 module.exports = {
-  hostUrl: isProdMode ? 'https://weixin.chinauff.com/spring' : 'http://weixin.chinauff.com/spring',
+  // hostUrl: isProdMode ? 'https://weixin.chinauff.com/spring' : 'http://weixin.chinauff.com/spring',
+  hostUrl: isProdMode ? 'https://weixin.chinauff.com/spring' : 'https://weixin.chinauff.com/spring',
   // hostUrl: isProdMode ? 'https://weixin.chinauff.com/spring' : 'http://wx.caixie.top/spring',
   cdnUrl: isProdMode ? 'https://weixin.chinauff.com/spring' : 'http://spring.chinauff.com/spring',
   proxyUrl: isProdMode ? 'https://weixin.chinauff.com/lnj-weixin/console' : 'http://demo.micvs.com/lnj-weixin/console',
-  baseUrl: isProdMode ? 'https://weixin.chinauff.com/spring/server/api' : 'http://spring.chinauff.com/api',
+  // baseUrl: isProdMode ? 'https://weixin.chinauff.com/spring/server/api' : 'http://spring.chinauff.com/api',
+  baseUrl: isProdMode ? 'https://weixin.chinauff.com/spring/server/api' : 'https://weixin.chinauff.com/spring/server/api',
   caixieUrl: isProdMode ? 'http://spring.chinauff.com/api' : 'http://spring.chinauff.com/api',
   thirdUrl: isProdMode ? 'https://weixin.chinauff.com/lnj-weixin/console' : 'http://demo.micvs.com/lnj-weixin/console',
   socketHost: isProdMode ? 'https://caixie.top' : 'http://wx.caixie.top'
