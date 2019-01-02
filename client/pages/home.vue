4c468963f9a03951e816417b920d28ed6741efb5
baisheng <baisheng@gmail.com>
2019-01-02 16:28:32 +0800

    To caixie test

--- client/pages/home.vue
+++ client/pages/home.vue
@@ -123,7 +123,7 @@
         wx.chooseImage({
           count: 1, // 默认9
           sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-          sourceType: ['album'],
+          sourceType: ['camera'],
           // sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: function (res) {
             const tempFilePaths = res.tempFilePaths
