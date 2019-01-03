06df4088757520dc5222a5c0a3952c6c91354bdc
baisheng <baisheng@gmail.com>
2019-01-03 19:18:59 +0800

    update wechat api

--- client/pages/home.vue
+++ client/pages/home.vue
@@ -127,12 +127,9 @@
           count: 1, // 默认9
           sizeType: ['original'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: ['camera'],
-          // sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: function (res) {
             const tempFilePaths = res.tempFilePaths
-            // console.log(tempFilePaths)
             const localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-            // console.log(localIds)
             self.localId = localIds[0]
 
             if (self.localId) {
