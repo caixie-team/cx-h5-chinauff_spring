a3088262414a602ba57a8f379c71d2255bbeb695
baisheng <baisheng@gmail.com>
2019-01-06 07:09:02 +0800

    update pro

---
+++ client/pages/index.first.vue
@@ -0,0 +1,71 @@
+<template>
+  <c-page>
+    <div
+      slot="content">
+      <div class="landing">
+        <img src="~assets/images/landing.png">
+      </div>
+    </div>
+  </c-page>
+</template>
+<script>
+  import CPage from '../components/c-page.vue'
+  import {isBrowser} from '~/environment_es'
+
+  export default {
+    name: 'Index',
+    head () {
+      return {
+        title: '老娘舅新春集福瓜分18吨福米'
+      }
+    },
+    components: {
+      CPage,
+    },
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .landing
+    display: flex
+    align-items: center
+    justify-content: center
+    flex-direction: column
+    min-height: 100vh
+    img
+      width: 640px
+      height: 1136px
+  .notices
+    color: #fff
+    display: flex
+    align-items: center
+    flex-direction: column
+    min-height: 100vh
+    h1
+      padding-top: 20px
+      font-size: 48px
+      font-weight: bold
+      line-height: 90px
+    p
+      font-size: $fontsize-large-xxx
+      font-weight: bold
+    .content
+      flex: 1
+      line-height: 50px
+      padding-top: 25%
+      text-align: center
+    .footer
+      display: flex
+      height: 350px
+      flex-direction: column
+      justify-content: center
+      align-items: center
+      line-height: 80px
+      color: #fff
+      img
+        width: 160px
+        height: 160px
+</style>
