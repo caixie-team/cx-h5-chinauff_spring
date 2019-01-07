1d2602b58654220341d653753682c7d23dbedfdb
baisheng <baisheng@gmail.com>
2019-01-07 09:55:03 +0800

    fix page621 lucky

--- client/store/index.js
+++ client/store/index.js
@@ -13,6 +13,7 @@ import apiConfig from '~/api.config.es'
 // import uaDevice from '~/utils/ua-device'
 import {isBrowser, isServer, isProdMode} from '~/environment_es'
 import {browserParse, osParse} from '~/utils/ua-os-browser'
+import axios from 'axios'
 
 // import shareIcon from '~/assets/img/icon/icon_gift.png'
 
@@ -26,8 +27,11 @@ const getResData = response => {
   // return response.status ? response.data : response
 }
 const resIsSuccess = response => {
+  // console.log('------')
+  console.log(response.data)
   // HTTP2
   if (response.data) {
+    // console.log('is right')
     return response.errno === 0 || response.errcode === 0 || response.errcode === '0' && response.data
     // return response.status && response.data && Object.is(response.data.code, 1)
   } else {
@@ -86,18 +90,6 @@ const clientInitWechatJSSDK = async (config, commit, beOpenId) => {
         imgUrl: img
       })
 
-      // w.callWechatApi('onMenuShareAppMessage', {
-      //   title: 'onMenuShareAppMessage test title',
-      //   type: 'link',
-      //   desc: 'onMenuShareAppMessage share description',
-      //   success: function () {
-      //     console.log('share on chat success')
-      //   },
-      //   cancel: function () {
-      //     console.log('share on chat canceled')
-      //   },
-      //   imgUrl: img
-      // })
       commit('user/REQUEST_USER_LOCATION')
       // 获取当前地理定位
       w.callWechatApi('getLocation', {
@@ -119,7 +111,35 @@ const clientInitWechatJSSDK = async (config, commit, beOpenId) => {
     })
     .catch(err => {
       commit('option/SET_LOG_INFO', err)
+      console.log('rest lgoin inf......')
+      wechatObj.signSignature({
+        'nonceStr': config.nonceStr,
+        'signature': config.signature,
+        'timestamp': config.timestamp
+      })
       console.error(err)
+    })
+}
+const loadJSSDKConfig = () => {
+  // console.log(url)
+  const url = window.location.href
+  // const getUrl = `/cx/wechat/signature?url=${encodeURIComponent(url)}`
+  const postUrl = `${API_PREFIX}/wechat/signature?url=${encodeURIComponent(url)}`
+  // console.log('LOAD JSSDK CONFIG...')
+  // console.log(postUrl)
+  // const postUrl = `/proxy/activity/weChat/getConfigMessage?appid=wxa8299eb7fc27ef04&url=` + encodeURIComponent(url)
+  return axios.post(postUrl)
+    .then(response => {
+      console.log(response.data)
+      // console.log(response.data)
+      if (response.data.errcode === '0' && response.data) {
+        return getResData(response.data)
+      } else {
+        console.log('微信签名信息获取失败：', response)
+      }
+    })
+    .catch(err => {
+      console.warn('获取签名信息错误', err)
     })
 }
 
@@ -153,16 +173,16 @@ export const actions = {
     // console.log(req.session.activity_user)
     store.commit('user/SET_USER_INFO', JSON.parse(req.session.activity_user))
     // console.log(getBaseUrl(req))
-    const initAppData = [
-      // 获取微信JSSDK配置
-      // store.dispatch('loadCxJSSDKConfig', getBaseUrl(req)),
-      store.dispatch('loadJSSDKConfig', getBaseUrl(req)),
-      // 获取 oauth 请求地址
-      // store.dispatch('loadOauthUrls')
-    ]
+    // const initAppData = [
+    // 获取微信JSSDK配置
+    // store.dispatch('loadCxJSSDKConfig', getBaseUrl(req)),
+    // store.dispatch('loadJSSDKConfig', getBaseUrl(req)),
+    // 获取 oauth 请求地址
+    // store.dispatch('loadOauthUrls')
+    // ]
 
     // 首次服务端渲染时渲
-    return Promise.all(initAppData)
+    // return Promise.all(initAppData)
   },
 
   /**
@@ -172,7 +192,48 @@ export const actions = {
    * @returns {Promise<void>}
    */
   async nuxtClientInit ({commit}, context) {
-    const jssdkConfig = JSON.parse(JSON.stringify(context.store.state.option.jssdkConfig))
+    console.log(context)
+    // store.dispatch('loadCxJSSDKConfig', getBaseUrl(req)),
+    // await store.dispatch('loadJSSDKConfig')
+    let jssdkConfig = {
+      // appId: 'wx40f58df735cd2868',
+      // appId: 'wxb44ce8b8c5cfdc0a',
+      // 老娘舅测试
+      // appId: 'wxa8299eb7fc27ef04',
+      // 老娘舅正式ID
+      appId: 'wxb44ce8b8c5cfdc0a',
+      nonceStr: '',
+      signature: '',
+      jsApiList: [
+        'hideMenuItems',
+        'onMenuShareTimeline',
+        'getLocation',
+        'chooseImage',
+        'previewImage',
+        'uploadImage',
+        'downloadImage'
+        // 'chooseCard',
+        // 'addCard',
+        // 'openCard',
+        // 'chooseWXPay'
+        // 'updateAppMessageShareData',
+        // 'updateTimelineShareData',
+      ],
+      url: ''
+    }
+    const loadConfig = await loadJSSDKConfig()
+    jssdkConfig = Object.assign({}, jssdkConfig, loadConfig)
+    jssdkConfig.appId = 'wxb44ce8b8c5cfdc0a'
+    jssdkConfig.jsApiList = [
+      'hideMenuItems',
+      'onMenuShareTimeline',
+      'getLocation',
+      'chooseImage',
+      'previewImage',
+      'uploadImage',
+      'downloadImage'
+    ]
+    // const jssdkConfig = JSON.parse(JSON.stringify(context.store.state.option.jssdkConfig))
     await clientInitWechatJSSDK(jssdkConfig, commit, this.getters.beOpenId)
   },
   // 获取 generateOAuthUrl 地址信息
@@ -201,13 +262,14 @@ export const actions = {
         console.warn('获取签名信息错误', err)
       })
   },
-  loadJSSDKConfig ({commit}, url) {
+  loadJSSDKConfig ({commit}) {
     // console.log(url)
+    const url = window.location.href
     // return this.$axios.$post(`${API_THIRD}/activity/weChat/getConfigMessage?url=${encodeURIComponent(url)}`)
     // const postUrl = `/proxy/activity/weChat/getConfigMessage?appid=wxa8299eb7fc27ef04&url=${encodeURIComponent(url)}`
     // const postUrl = `/proxy/activity/weChat/getConfigMessage?appid=wxb44ce8b8c5cfdc0a&url=${encodeURIComponent(url)}`
     const postUrl = `http://crm.chinauff.com/lnj-weixin/console/activity/weChat/getConfigMessage?appid=wxb44ce8b8c5cfdc0a&url=${encodeURIComponent(url)}`
-
+    console.log('LOAD JSSDK CONFIG...')
     console.log(postUrl)
     // const postUrl = `/proxy/activity/weChat/getConfigMessage?appid=wxa8299eb7fc27ef04&url=` + encodeURIComponent(url)
     return this.$axios.$post(postUrl)
@@ -562,8 +624,9 @@ export const actions = {
   // 好友助力
   async dealHelpAction ({commit}, params) {
     const res = await this.$axios.$post(`${API_PREFIX}/blessing/help`, {
-      openId: this.getters.openId,
-      beOpenId: params.beOpenId
+      ...params
+      // openId: this.getters.openId,
+      // beOpenId: params.beOpenId
     })
     if (res && res.errno === 0) {
       return true
