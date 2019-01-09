482008dac4bd8f03d3b6d61297e6f4a15449fa3b
baisheng <baisheng@gmail.com>
2019-01-09 23:56:41 +0800

    update middlware

--- src/middleware/chinauff.js
+++ src/middleware/chinauff.js
@@ -27,7 +27,6 @@ module.exports = options => {
       const openId = await ctx.session('openId') || await ctx.cookie('openId')
       const query = ctx.query
       const encodeURI = encodeURIComponent(`${apiConfig.domain}${ctx.req.url}`)
-      // const callbackUrl = `${apiConfig.proxyUrl}/activity/weChat/openId?callback=${encodeURI}`
       const callbackUrl = `http://crm.chinauff.com/lnj-weixin/console/activity/weChat/openId?callback=${encodeURI}`
       // 请求地址中不包含 openId
       // myOpenId 跳转回来的用于验证
