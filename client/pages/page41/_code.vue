97c455c2fc85d3d1fce3c94b3349d10afd9d582d
baisheng <baisheng@gmail.com>
2019-01-10 18:22:18 +0800

    fix 预约状态显示

--- client/pages/page41/_code.vue
+++ client/pages/page41/_code.vue
@@ -187,12 +187,12 @@
       async selectHandle (date, selectedVal, selectedText) {
         console.log(selectedText)
         console.log(selectedVal)
-        const days = (new Date(selectedVal) - new Date(new Date())) / 1000 / 60 / 60 / 24
-        if (days < 3) {
-          EventBus.$emit('err-msg', '日份最早可选择第三天的日子')
-          return
-        }
-
+        // const days = (new Date(selectedVal) - new Date(new Date())) / 1000 / 60 / 60 / 24
+        // if (days < 3) {
+        //   EventBus.$emit('err-msg', '日份最早可选择第三天的日子')
+        //   return
+        // }
+        //
         this.selectedDate = selectedText.join('')
         await this.$store.commit('user/SET_RESERVER_FORM', {
           reserve_date: new Date(selectedVal).getTime(),
