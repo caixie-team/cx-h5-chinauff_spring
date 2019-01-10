d8cfe6f85058f50a75a31af93fd96c19a81b7140
baisheng <baisheng@gmail.com>
2019-01-10 22:50:09 +0800

    优化

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -41,7 +41,7 @@ module.exports = {
     // ],
     meta: [
       {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {name: 'viewport', content: '      width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'},
       // { name: 'viewport', content: ' width = device-width，initial-scale = 1，maximum-scale = 1，minimum-scale = 1，user-scalable = no' },
       {hid: 'description', name: 'description', content: pkg.description},
       {'http-equiv': 'pragma', content: 'no-cache'},
